af22c2cd8b0ea40a34042bc220c73e322836481f
Dmitry <mezentsevdmitry51@gmail.com>
2018-07-02 00:47:19 +0500

    fix

--- src/upload/catalog/view/javascript/dd-widget-init.js
+++ src/upload/catalog/view/javascript/dd-widget-init.js
@@ -9,7 +9,7 @@
             document.cookie = name + '=' + (value || '')  + '; expires=' + date.toUTCString() + '; path=/';
         },
         remove: function (name) {
-            document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+            document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
         }
     };
 
