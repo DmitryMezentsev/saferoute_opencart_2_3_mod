015d53f3c267a736bb78e6aa5af7e68fd79062ab
kjoke <kjoke@ya.ru>
2020-11-09 22:40:37 +0300

    add deliveryType and delivery company name

--- src/install.php
+++ src/install.php
@@ -11,8 +11,15 @@ function checkCol($db, $name)
 }
 
 
-if (!checkCol($this->db, 'saferoute_id'))
-    $this->db->query("ALTER TABLE `".DB_PREFIX."order` ADD `saferoute_id` char(48) NOT NULL");
-
-if (!checkCol($this->db, 'in_saferoute_cabinet'))
-    $this->db->query("ALTER TABLE `".DB_PREFIX."order` ADD `in_saferoute_cabinet` TINYINT(1) DEFAULT 0");
+if (!checkCol($this->db, 'saferoute_id')) {
+    $this->db->query("ALTER TABLE `" . DB_PREFIX . "order` ADD `saferoute_id` char(48) NOT NULL");
+}
+if (!checkCol($this->db, 'in_saferoute_cabinet')) {
+    $this->db->query("ALTER TABLE `" . DB_PREFIX . "order` ADD `in_saferoute_cabinet` TINYINT(1) DEFAULT 0");
+}
+if (!checkCol($this->db, 'saferoute_delivery_company')) {
+    $this->db->query("ALTER TABLE `" . DB_PREFIX . "order` ADD `saferoute_delivery_company` VARCHAR(255) NOT NULL");
+}
+if (!checkCol($this->db, 'saferoute_delivery_type')) {
+    $this->db->query("ALTER TABLE `" . DB_PREFIX . "order` ADD `saferoute_delivery_type` TINYINT(1) DEFAULT 0");
+}
